b7a952a13db44e2101e375d21837bd1e15c854a0
jimmypifpaf <gid_94@tlen.pl>
2015-05-01 19:58:13 +0200

    Create functions_bstrzebonski.h

---
+++ functions/bstrzebonski/functions_bstrzebonski.h
@@ -0,0 +1,23 @@
+/**
+ ** Deklaracje funkcji
+ ** Autor pliku : Bartłomiej Strzeboński
+ **
+ **/
+
+// zabezpieczenie przed wielokrotnym wczytaniem pliku
+#ifndef functions_bstrzebonski_h
+#define functions_bstrzebonski_h
+
+extern int exit_program;
+
+/**
+ ** Krótki opis/nazwa funkcji. Opis danych wejsciowych i zwracanych
+ **/
+
+void function_bartek(void);
+
+/**
+ ** kolejne funkcje ...
+ **/
+
+#endif
