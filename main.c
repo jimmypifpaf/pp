6e234c18198c31eb098200a9728430a4023bec54
jimmypifpaf <gid_94@tlen.pl>
2015-05-01 20:16:30 +0200

    Revert "Update main.c"
    This reverts commit 39d1fdfe0307deffcafc7743dadc1e5edf78e864.

--- main.c
+++ main.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <conio.h>
-
-#include "functions/ppiechowicz/functions_ppiechowicz.h"
-#include "functions/bstrzebonski/functions_bstrzebonski.h"
-#include "functions/jpieniazek/functions_jpieniazek.h"
-#include "functions/kzemczak/functions_kzemczak.h"
+#include "functions/functions.h"
 
 int exit_program = 0;
 
@@ -14,13 +10,9 @@ int main(void) {
     show_msg_welcome();
     login();
 
-    function_bartek();
-    function_jarek();
-    function_krzysiek();
-
-    while( !exit_program )
+    while(!exit_program)
          {
-            show_menu("main");
+    	       show_menu("main");
          }
 
     show_msg_exit();
