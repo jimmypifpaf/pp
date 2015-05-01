39d1fdfe0307deffcafc7743dadc1e5edf78e864
jimmypifpaf <gid_94@tlen.pl>
2015-05-01 19:53:16 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <conio.h>
-#include "functions/functions.h"
+
+#include "functions/ppiechowicz/functions_ppiechowicz.h"
+#include "functions/bstrzebonski/functions_bstrzebonski.h"
+#include "functions/jpieniazek/functions_jpieniazek.h"
+#include "functions/kzemczak/functions_kzemczak.h"
 
 int exit_program = 0;
 
@@ -10,9 +14,13 @@ int main(void) {
     show_msg_welcome();
     login();
 
-    while(!exit_program)
+    function_bartek();
+    function_jarek();
+    function_krzysiek();
+
+    while( !exit_program )
          {
-    	       show_menu("main");
+            show_menu("main");
          }
 
     show_msg_exit();
