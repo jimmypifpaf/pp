bcce3361084f7947c274efea9bd866858d526b56
jimmypifpaf <gid_94@tlen.pl>
2015-05-01 20:02:45 +0200

    Create functions_kzemczak.c

---
+++ functions/kzemczak/functions_kzemczak/functions_kzemczak.c
@@ -0,0 +1,26 @@
+/**
+ ** Definicje funkcji
+ ** Autor pliku : Krzysztof Zemczak
+ **
+ **/
+
+#include <stdio.h>
+#include <string.h>
+
+#include "functions_kzemczak.h"
+#include "../bstrzebonski/functions_bstrzebonski.h"
+#include "../jpieniazek/functions_jpieniazek.h"
+#include "../ppiechowicz/functions_ppiechowicz.h"
+
+/**
+ ** Krótki opis/nazwa funkcji. Opis danych wejsciowych i zwracanych
+ **/
+
+void function_krzysiek(void) {
+    printf("Funkcja Krzyka.\n");
+}
+
+/**
+ ** kolejne funkcje ...
+ **/
+
