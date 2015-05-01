491a4b155a4db67be7f5bd6844a275b5cedb9192
jimmypifpaf <gid_94@tlen.pl>
2015-05-01 19:59:47 +0200

    Create functions_bstrzebonski.c

---
+++ functions/bstrzebonski/functions_bstrzebonski.c
@@ -0,0 +1,26 @@
+/**
+ ** Definicje funkcji
+ ** Autor pliku : Bartłomiej Strzeboński
+ **
+ **/
+
+#include <stdio.h>
+#include <string.h>
+
+#include "functions_bstrzebonski.h"
+#include "../ppiechowicz/functions_ppiechowicz.h"
+#include "../jpieniazek/functions_jpieniazek.h"
+#include "../kzemczak/functions_kzemczak.h"
+
+/**
+ ** Krótki opis/nazwa funkcji. Opis danych wejsciowych i zwracanych
+ **/
+
+void function_bartek(void) {
+    printf("Funkcja Bartka.\n");
+}
+
+/**
+ ** kolejne funkcje ...
+ **/
+
