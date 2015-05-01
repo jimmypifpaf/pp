1a7098ca2ae8da261599b0ec5910c1e6ec0c35fa
jimmypifpaf <gid_94@tlen.pl>
2015-05-01 20:01:05 +0200

    Create functions_jpieniazek.c

---
+++ functions/jpieniazek/functions_jpieniazek.c
@@ -0,0 +1,26 @@
+/**
+ ** Definicje funkcji
+ ** Autor pliku : Jarosław Pieniążek
+ **
+ **/
+
+#include <stdio.h>
+#include <string.h>
+
+#include "functions_jpieniazek.h"
+#include "../bstrzebonski/functions_bstrzebonski.h"
+#include "../kzemczak/functions_kzemczak.h"
+#include "../ppiechowicz/functions_ppiechowicz.h"
+
+/**
+ ** Krótki opis/nazwa funkcji. Opis danych wejsciowych i zwracanych
+ **/
+
+void function_jarek(void) {
+    printf("Funkcja Jarka.\n");
+}
+
+/**
+ ** kolejne funkcje ...
+ **/
+
