c5c1097a7c5ae89c2a4222e0b3950ebdd2435fcf
jimmypifpaf <gid_94@tlen.pl>
2015-05-01 20:00:45 +0200

    Create functions_jpieniazek.h

---
+++ functions/jpieniazek/functions_jpieniazek.h
@@ -0,0 +1,23 @@
+/**
+ ** Deklaracje funkcji
+ ** Autor pliku : Jarosław Pieniążek
+ **
+ **/
+
+// zabezpieczenie przed wielokrotnym wczytaniem pliku
+#ifndef functions_jpieniazek_h
+#define functions_jpieniazek_h
+
+extern int exit_program;
+
+/**
+ ** Krótki opis/nazwa funkcji. Opis danych wejsciowych i zwracanych
+ **/
+
+void function_jarek(void);
+
+/**
+ ** kolejne funkcje ...
+ **/
+
+#endif
