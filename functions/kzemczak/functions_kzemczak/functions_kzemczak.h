84dd1f8a9054b15b5f1a4a9d5d879565b9a82f3c
jimmypifpaf <gid_94@tlen.pl>
2015-05-01 20:02:11 +0200

    Create functions_kzemczak.h

---
+++ functions/kzemczak/functions_kzemczak/functions_kzemczak.h
@@ -0,0 +1,23 @@
+/**
+ ** Deklaracje funkcji
+ ** Autor pliku : Krzysztof Zemczak
+ **
+ **/
+
+// zabezpieczenie przed wielokrotnym wczytaniem pliku
+#ifndef functions_kzemczak_h
+#define functions_kzemczak_h
+
+extern int exit_program;
+
+/**
+ ** Krótki opis/nazwa funkcji. Opis danych wejsciowych i zwracanych
+ **/
+
+void function_krzysiek(void);
+
+/**
+ ** kolejne funkcje ...
+ **/
+
+#endif
