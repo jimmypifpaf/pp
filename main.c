b126a47b878dc5354bb0c04d50c7aa2824366745
jimmypifpaf <gid_94@tlen.pl>
2015-05-01 18:47:46 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include "functions/functions.h"
+
+int exit_program = 0;
+
+int main(void) {
+
+	show_msg_welcome();
+    login();
+
+    while(!exit_program)
+         {
+    	       show_menu("main");
+         }
+
+    show_msg_exit();
+    return 0;
+}
