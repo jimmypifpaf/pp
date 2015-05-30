113b4e56f8f08213a103d391ec2cb1590e113490
jimmypifpaf <gid_94@tlen.pl>
2015-05-30 10:45:14 +0200

    Update functions_ppiechowicz.c

--- functions/ppiechowicz/functions_ppiechowicz.c
+++ functions/ppiechowicz/functions_ppiechowicz.c
@@ -58,49 +58,12 @@ void show_menu(char *menu_name) {
 }
 
 /**
- ** Oczekiwanie na wybranie pozycji menu
- ** - walidacja wprowadzonej cyfry
- **
- ** Funkcja zwraca wprowadzoną, sprawdzoną cyfrę [int]
- **/
-
-int select_menu_option(int *possibility_vals, int posibility_vals_size) {
-    int result, operation, c, i;
-
-    do {
-            result = scanf("%d", &operation);
-
-            if( result )
-              {
-                  result = 0;
-                  for( i = 0; i < posibility_vals_size; i++ )
-                     {
-                          if( operation == possibility_vals[i] )
-                              result = 1;
-                     }
-              }
-
-            if( !result )
-              {
-                  printf("Nieznana akcja! Prosze wpisać cyfrę podaną w menu.");
-                  fflush(stdout);
-
-                  // czyszczenie bufora znak po znaku
-                  while( (c = getchar()) != '\n' && c != EOF );
-              }
-        } while( result != 1 );
-
-    return operation;
-}
-
-/**
  ** rysuj menu :: menu glowne
  **/
 
 void drow_menu_main(void) {
 
-    int possibility_vals[] = { 1, 2 }, // tablica poprawnych wartosci, ktore moga zostac wprowadzone
-            selected_option; // wybrana opcja
+    int selected_option = 0; // wybrana opcja
 
     printf("\n---------------------------------------------\n");
     printf("**************** MENU GŁÓWNE ***************");
@@ -112,8 +75,8 @@ void drow_menu_main(void) {
     printf("Akcja: ");
     fflush(stdout);
 
-    selected_option = select_menu_option(possibility_vals, sizeof(possibility_vals)); // oczekiwanie na wybranie opcji
-    printf("wybrano: %d", selected_option); // tutaj zamiast printfa switch, który bedzie wywolywal odpowiednie funkcje, np. :
+    scanf("%d", &selected_option);
+    printf("wybrano: %d", selected_option);
 
     switch( selected_option )
           {
@@ -122,6 +85,9 @@ void drow_menu_main(void) {
               break;
 
               case 2 : exit_program = 1;
+              break;
+
+              default: drow_menu_main();
               break;
           }
 }
@@ -132,21 +98,33 @@ void drow_menu_main(void) {
 
 void drow_menu_example1(void) {
 
-    int possibility_vals[] = { 1, 2 }, // tablica poprawnych wartosci, ktore moga zostac wprowadzone
-        selected_option;               // wybrana opcja
+    int selected_option = 0; // wybrana opcja
 
     printf("\n---------------------------------------------\n");
-    printf("************** PRZYKŁADOWE MENU **************");
+    printf("*************** MENU PRZYKŁADOWE **************");
     printf("\n---------------------------------------------\n\n");
 
-    printf("1. Opcja 1.\n"
-           "2. Opcja 2.\n");
+    printf("1. Przegladaj przyloty.\n"
+           "2. Powrót do menu głównego.\n");
 
     printf("Akcja: ");
     fflush(stdout);
 
-    selected_option = select_menu_option(possibility_vals, sizeof(possibility_vals)); // oczekiwanie na wybranie opcji
-    printf("wybrano: %d", selected_option); // tutaj zamiast printfa switch, który bedzie wywolywal odpowiednie funkcje.
+    scanf("%d", &selected_option);
+    printf("wybrano: %d", selected_option);
+
+    switch( selected_option )
+          {
+              case 1 : printf("przegladaj przyloty.");
+                       show_menu("example");
+              break;
+
+              case 2 : return;
+              break;
+
+              default: drow_menu_example1();
+              break;
+          }
 }
 
 /**
